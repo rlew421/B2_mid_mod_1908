3c221dc81b9b3a9e26d0e9228b3ec0bbac49e966
Rachel Lew <rlew07@gmail.com>
2019-10-20 23:08:27 -0600

    Adds average student age method

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -2,4 +2,8 @@ class Instructor < ApplicationRecord
   validates_presence_of :name
   has_many :instructor_students
   has_many :students, through: :instructor_students
+
+  def average_student_age
+    students.average(:age).to_f.round(2)
+  end
 end
