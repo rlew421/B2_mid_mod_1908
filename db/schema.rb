f13fb4c9f2d855167f29cf5aa9af64b907bbf933
Rachel Lew <rlew07@gmail.com>
2019-10-20 20:16:06 -0600

    Creates instructor_students joins table and migrates into database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_014804) do
+ActiveRecord::Schema.define(version: 2019_10_21_020417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "instructor_students", force: :cascade do |t|
+    t.bigint "student_id"
+    t.bigint "instructor_id"
+    t.index ["instructor_id"], name: "index_instructor_students_on_instructor_id"
+    t.index ["student_id"], name: "index_instructor_students_on_student_id"
+  end
 
   create_table "instructors", force: :cascade do |t|
     t.string "name"
@@ -25,4 +32,6 @@ ActiveRecord::Schema.define(version: 2019_10_21_014804) do
     t.integer "age"
   end
 
+  add_foreign_key "instructor_students", "instructors"
+  add_foreign_key "instructor_students", "students"
 end
